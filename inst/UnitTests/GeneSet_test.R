2d470ea6fa18690dd8e62eec17a82f7e86e6c319
Seth Falcon <sfalcon@fhcrc.org>
2007-05-17 23:56:19 +0000

    Add start of basic test for GeneSet
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GSEABase@24787 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/UnitTests/GeneSet_test.R
@@ -0,0 +1,15 @@
+test_DefaultConstructor <- function() {
+    gs <- GeneSet(genes=letters[1:5],
+                  setIdentifier="unique!",
+                  setName="TestSet",
+                  shortDescription="Test Gene Set No. 1",
+                  longDescription="This is a gene set used for testing.",
+                  organism="AlienX5.11",
+                  pubMedIds=c("1", "2"),
+                  urls=c("http://bioconductor.org"),
+                  contributor="A.U. Thor")
+
+    ## Basic accessor testing
+    checkEquals("unique!", setIdentifier(gs))
+    checkEquals("TestSet", setName(gs))
+}

26d77835762e3b4dfb400c069156b7153914bc99
Martin Morgan <mtmorgan@fhcrc.org>
2015-04-17 05:19:19 +0000

    support 'h' MSigDB gene set
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GSEABase@102627 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-CollectionType.R
+++ R/methods-CollectionType.R
@@ -70,7 +70,7 @@ setMethod("setdiff",
 
 BroadCollection <- function(category="c1", subCategory=NA, ...) {
     if (length(category)!=1 ||
-        !(category %in% c("c1", "c2", "c3", "c4", "c5", "c6", "c7")))
+        !(category %in% c("c1", "c2", "c3", "c4", "c5", "c6", "c7", "h")))
         stop(sprintf("invalid BroadCollection category: '%s'",
                      paste(category, collapse="', '")))
     new("BroadCollection",
@@ -93,9 +93,10 @@ setMethod("show",
                          c2="c2 (Curated)",
                          c3="c3 (Motif)",
                          c4="c4 (Computational)",
-                         c5="c5 (GO)", 
+                         c5="c5 (GO)",
                          c6="c6 (Oncogenic Pathway Activation Modules)",
-                         c7="c7 (Immunologic Signatures)"), "\n",
+                         c7="c7 (Immunologic Signatures)"),
+                         h="h (Hallmark)", "\n",
                   "  bcSubCategory:  ", bcSubCategory(object), "\n", sep="")
           })
 
